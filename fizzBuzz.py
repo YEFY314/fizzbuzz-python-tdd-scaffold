46b7b61c8a859e32ef8e59f93396ebe73a96ba09
YEFY314 <yefengyu.314@qq.com>
2020-01-14 16:17:32 +0800

    pycodestyle修改缩进样式

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,25 +1,26 @@
 def canBedivide(dividend, *dividor):
-  result = True
-  for i in dividor:
-    if int(dividend) % int(i) !=0:
-      result = False
-  return result
+    result = True
+    for i in dividor:
+        if int(dividend) % int(i) != 0:
+            result = False
+    return result
 
 
 def fizzBuzz(num=100):
-  result = []
-  for i in range(int(num)):
-    i = i+1
-    if canBedivide(i, 3, 5):
-      temp = "FizzBuzz"
-    elif canBedivide(i, 3):
-      temp = "Fizz"
-    elif canBedivide(i, 5):
-      temp = "Buzz"
-    else:
-      temp = i
-    print(temp)
-    result.append(temp)
-  return result
+    result = []
+    for i in range(int(num)):
+        i = i+1
+        if canBedivide(i, 3, 5):
+            temp = "FizzBuzz"
+        elif canBedivide(i, 3):
+            temp = "Fizz"
+        elif canBedivide(i, 5):
+            temp = "Buzz"
+        else:
+            temp = i
+        print(temp)
+        result.append(temp)
+    return result
+
 
 fizzBuzz()
